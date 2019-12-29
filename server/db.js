f079f2926594389a01958f15a1baa2a69a14a86b
Tony Cheang <cheangytony@gmail.com>
2019-12-29 00:39:57 -0800

    Removed db.js since this project now uses Knex.

--- server/db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require("pg");
-const pool = new Pool();
-
-module.exports = {
-    query: async (text, params) => await pool.query(text, params),
-    // Connect used to return a client for transactions.
-    connect: async () => await pool.connect(),
-}
