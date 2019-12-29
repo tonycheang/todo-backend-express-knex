88bbbe9f325706c10e6e7429f023cb2015f22ad8
Tony Cheang <cheangytony@gmail.com>
2019-12-29 00:55:21 -0800

    Split server.js into server-config.js and server.js for organization.

--- server/server.js
+++ server/server.js
@@ -1,23 +1,6 @@
-require('dotenv').config()
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const bodyParser = require('body-parser');
+const app = require('./server-config.js');
 
-const app = express();
 const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 app.get('/', (req, res) => {
   console.log("root")
