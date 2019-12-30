e1f799f14d994e9c4c9f3792a47a6b01589857b9
Tony Cheang <cheangytony@gmail.com>
2019-12-29 18:24:48 -0800

    Added database queries and routes that pass the todo-backend specification.

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,6 +2,22 @@
 require('dotenv').config()
 module.exports = {
 
+  test: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.PGDATABASE,
+      user:     process.env.PGUSER,
+      password: process.env.PGPASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
   development: {
     client: 'postgresql',
     connection: {
--- server/server.js
+++ server/server.js
@@ -1,13 +1,52 @@
+const _ = require('lodash');
 const app = require('./server-config.js');
+const todos = require('./database/queries.js');
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  console.log("root")
-  res.send({
-    express: 'Hello From Express'
-  });
+function createToDo(req, data) {
+  const protocol = req.protocol, 
+    host = req.get('host'), 
+    id = data.id;
+
+  return {
+    title: data.title,
+    order: data.order,
+    completed: data.completed || false,
+    url: `${protocol}://${host}/${id}`
+  };
+}
+
+app.get('/', async (req, res) => {
+  const allEntries = await todos.all();
+  return res.send(allEntries.map( _.curry(createToDo)(req) ));
 });
+
+app.get('/:id', async (req, res) => {
+  const todo = await todos.get(req.params.id);
+  return res.send(todo);
+});
+
+app.post('/', async (req, res) => {
+  const created = await todos.create(req.body.title, req.body.order);
+  return res.send(createToDo(req, created));
+});
+
+app.patch('/:id', async (req, res) => {
+  const patched = await todos.update(req.params.id, req.body);
+  return res.send(createToDo(req, patched));
+});
+
+app.delete('/', async (req, res) => {
+  const deletedEntries = await todos.clear();
+  return res.send(deletedEntries.map( _.curry(createToDo)(req) ));
+});
+
+app.delete('/:id', async (req, res) => {
+  const deleted = await todos.delete(req.params.id);
+  return res.send(createToDo(req, deleted));
+});
+
 
 app.get('/api/hello', (req, res) => {
   res.send({
