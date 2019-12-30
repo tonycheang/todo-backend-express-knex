f2ecce2e7fc0931f89b006ee8562e25363352392
Tony Cheang <cheangytony@gmail.com>
2019-12-30 01:26:56 -0800

    Added del and patch functions to accompany existing post and get.

--- server/tests/util/httpRequests.js
+++ server/tests/util/httpRequests.js
@@ -20,8 +20,28 @@ function get(url) {
   return httpRequest;
 }
 
+// a helper function to make a PATCH request.
+function patch(url, body) {
+  const httpRequest = request(app).patch(url);
+  httpRequest.send(body);
+  httpRequest.set('Accept', 'application/json')
+  httpRequest.set('Origin', 'http://localhost:5000')
+  return httpRequest;
+}
+
+// a helper function to make a DELETE request.
+function del(url) {
+  const httpRequest = request(app).delete(url);
+  httpRequest.set('Accept', 'application/json')
+  httpRequest.set('Origin', 'http://localhost:5000')
+  return httpRequest;
+}
+
 
 module.exports = {
   post,
   get,
+  patch,
+  del,
+  delete: del
 };
