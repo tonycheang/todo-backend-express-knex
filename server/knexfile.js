705883980f5956690efb46e2fe1f3e9604fd3f97
Tony Cheang <cheangytony@gmail.com>
2019-12-30 01:28:52 -0800

    Added comment to top about flexibility and also concerns changing databases using knex.

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,5 +1,10 @@
-// Update with your config settings.
-require('dotenv').config()
+/* 
+  Update with your config settings.
+  The test database and development database are by default the same.
+  Knex also allows for easy switching between databases. 
+  But the .returning() method will only work for PostgreSQL, MSSQL, and Oracle databases.
+*/
+require('dotenv').config();
 module.exports = {
 
   test: {
