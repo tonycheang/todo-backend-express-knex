a9083e6145490a8eb881cf2395c4aad82a9bfcf1
Tony Cheang <cheangytony@gmail.com>
2019-12-30 03:17:28 -0800

    Removed get /api/hello and post /api/world routes.

--- server/server.js
+++ server/server.js
@@ -12,19 +12,6 @@ app.patch('/:id', routes.patchTodo);
 app.delete('/', routes.deleteAllTodos);
 app.delete('/:id', routes.deleteTodo);
 
-
-app.get('/api/hello', (req, res) => {
-  res.send({
-    express: 'Hello From Express'
-  });
-});
-
-app.post('/api/world', (req, res) => {
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Listening on port ${port}`));
 }
