02315d6b166f88898812e77bd380351ed846fb76
Tony Cheang <cheangytony@gmail.com>
2019-12-30 01:59:00 -0800

    Abstracted routes into a separate file to allow for adding additional features like error reporting.

--- server/server.js
+++ server/server.js
@@ -1,51 +1,16 @@
-const _ = require('lodash');
 const app = require('./server-config.js');
-const todos = require('./database/queries.js');
+const routes = require('./server-routes.js');
 
 const port = process.env.PORT || 5000;
 
-function createToDo(req, data) {
-  const protocol = req.protocol, 
-    host = req.get('host'), 
-    id = data.id;
+app.get('/', routes.getAllTodos);
+app.get('/:id', routes.getTodo);
 
-  return {
-    title: data.title,
-    order: data.order,
-    completed: data.completed || false,
-    url: `${protocol}://${host}/${id}`
-  };
-}
+app.post('/', routes.postTodo);
+app.patch('/:id', routes.patchTodo);
 
-app.get('/', async (req, res) => {
-  const allEntries = await todos.all();
-  return res.send(allEntries.map( _.curry(createToDo)(req) ));
-});
-
-app.get('/:id', async (req, res) => {
-  const todo = await todos.get(req.params.id);
-  return res.send(todo);
-});
-
-app.post('/', async (req, res) => {
-  const created = await todos.create(req.body.title, req.body.order);
-  return res.send(createToDo(req, created));
-});
-
-app.patch('/:id', async (req, res) => {
-  const patched = await todos.update(req.params.id, req.body);
-  return res.send(createToDo(req, patched));
-});
-
-app.delete('/', async (req, res) => {
-  const deletedEntries = await todos.clear();
-  return res.send(deletedEntries.map( _.curry(createToDo)(req) ));
-});
-
-app.delete('/:id', async (req, res) => {
-  const deleted = await todos.delete(req.params.id);
-  return res.send(createToDo(req, deleted));
-});
+app.delete('/', routes.deleteAllTodos);
+app.delete('/:id', routes.deleteTodo);
 
 
 app.get('/api/hello', (req, res) => {
