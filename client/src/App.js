66d2739fda2658216e7b2d2cc58b6820b6ce3fe0
Tony Cheang <cheangytony@gmail.com>
2019-12-30 21:27:33 -0800

    Added form for submitting requests to todo-backend, postman-like.

--- client/src/App.js
+++ client/src/App.js
@@ -6,69 +6,150 @@ import './App.css';
 
 class App extends Component {
   state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    
-    return body;
+    method: 'GET',
+    lastRequest: '',
+
+    id: '',
+    title: '',
+    order: '',
+    completed: false,
+
+    response: [],
   };
   
   handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
+    let { method, id, title, order, completed } = this.state;
+    
+    let request = {
+      method,
       headers: {
         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    
-    this.setState({ responseToPost: body });
+      }
+    };
+
+    // Undefined ensures not changing to empty string.
+    title = title ? title : undefined;
+    order = order ? Number(order) : undefined;
+
+    if (method !== "GET")
+      request.body = JSON.stringify({ title, order, completed })
+
+    this.setState({ lastRequest: `${method} at /${id}`});
+    // Code smells, but the setup of todo-backend with get('/') returning a list of todos requires
+    // that we directly hit localhost instead of being able to rely on the proxy.
+    // We can only proxy non-root gets.
+    let response;
+    if (process.env.NODE_ENV === "development" && method === "GET" && id === '') {
+      response = await fetch('http://localhost:5000/', request);
+    } else {
+      response = await fetch(`/${id}`, request);
+    }
+
+    const contentType = response.headers.get('content-type');
+
+    let body;
+    if (contentType && contentType.includes('application/json')) {
+      body = await response.json();
+    } else if (contentType && contentType.includes('text/html')) {
+      body = await response.text();
+    }
+
+    if (response.status !== 200) {
+      console.log(body);
+      this.setState({ response: [{ status: response.status, message: body }] });
+      return;
+    }
+
+    // Ensures formart of [{}, {}, {}]
+    if (!Array.isArray(body))
+      body = Array(body);
+  
+    this.setState({ response: body });
+  };
+
+  changeMethod = event => {
+    this.setState({ method: event.target.value });
   };
   
-render() {
+  render() {
+    const { method, lastRequest, id, title, order, completed, response } = this.state;
+
+    const shouldDisplayIdInput = method !== "POST";
+    const shouldDisplayTitleInput = method === "POST" || method === "PATCH";
+    const shouldDisplayOrderInput = method === "POST" || method === "PATCH";
+    const shouldDisplayCompletedInput = method === "PATCH";
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Powered by React
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-        <p>{this.state.response}</p>
+
         <form onSubmit={this.handleSubmit}>
           <p>
-            <strong>Post to Server:</strong>
+            <h3>Send to Server:</h3>
           </p>
+          <select value={method} onChange={this.changeMethod}>
+            <option value="GET">Get</option>
+            <option value="POST">Post</option>
+            <option value="PATCH">Patch</option>
+            <option value="DELETE">Delete</option>
+          </select>
           <input
+            disabled={!shouldDisplayIdInput}
             type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
+            placeholder="id (int)"
+            value={id}
+            onChange={e => this.setState({ id: e.target.value })}
           />
+          <input
+            disabled={!shouldDisplayTitleInput}
+            type="text"
+            placeholder="title (string)"
+            value={title}
+            onChange={e => this.setState({ title: e.target.value })}
+          />
+          <input
+            disabled={!shouldDisplayOrderInput}
+            type="text"
+            placeholder="order (int)"
+            value={order}
+            onChange={e => this.setState({ order: e.target.value })}
+          />
+
+          <label>
+            Completed?
+            <input
+              display="inline-block"
+              disabled={!shouldDisplayCompletedInput}
+              type="checkbox"
+              value={completed}
+              onChange={e => this.setState({ completed: e.target.checked })}
+            />
+          </label>
+
           <button type="submit">Submit</button>
         </form>
-        <p>{this.state.responseToPost}</p>
+        <h3>{`Last sent: ${lastRequest}`}</h3>
+        <p>
+          {
+            response.map((todo, i) => {
+              return (
+                <li key={i}>
+                  { 
+                    todo ? Object.entries(todo).map(([key, value]) => {
+                      return `${key}: ${value}   `
+                    }) : undefined
+                  }
+                </li>
+              )
+            })
+          }
+        </p>
       </div>
     );
   }
